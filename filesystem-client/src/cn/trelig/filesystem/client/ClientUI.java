bd76b268db0de2de011465a06f9c55e7c9acc9a6
trelig <2432216839@qq.com>
2019-05-31 15:14:42 +0800

    在保证完整功能的基础上，实现了多线程传输文件。

--- filesystem-client/src/cn/trelig/filesystem/client/ClientUI.java
+++ filesystem-client/src/cn/trelig/filesystem/client/ClientUI.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
